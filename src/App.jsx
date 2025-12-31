7cf57f0bc368565e23e121aa9060a25ff44e5828
tayyab26x <tayyab26x@gmail.com>
2025-12-31 11:23:05 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Tailwind v4 working
+      </h1>
+    </div>
+  )
+}
+
+export default App
